511b56f698505383069e076af9c2cc79d58ad054
Stephen Tu <tu.stephenl@gmail.com>
2016-03-20 16:30:55 -0700

    give branch hints

--- src/cpp/instance.cpp
+++ src/cpp/instance.cpp
@@ -12,6 +12,9 @@
 
 #include "xorshift.hpp"
 
+#define likely(x)   __builtin_expect(!!(x), 1)
+#define unlikely(x) __builtin_expect(!!(x), 0)
+
 using namespace std;
 
 static void sampleGraphWithGroundTruth(
@@ -30,7 +33,7 @@ static void sampleGraphWithGroundTruth(
   //xorshift prng(seed);
   default_random_engine prng(seed);
   bernoulli_distribution faircoin(0.5);
-  uniform_real_distribution<double> unif(0.0, 1.0);
+  uniform_real_distribution<float> unif(0.0, 1.0);
 
   x0.clear();
   x0.reserve(n);
@@ -39,17 +42,17 @@ static void sampleGraphWithGroundTruth(
   for (unsigned i = 0; i < n; i++)
     x0.push_back(faircoin(prng) ? +1.0 : -1.0);
 
-  const double inp = a/static_cast<double>(n);
-  const double outp = b/static_cast<double>(n);
+  const float inp = a/static_cast<float>(n);
+  const float outp = b/static_cast<float>(n);
 
   for (unsigned src = 0; src < n; src++) {
     const int x0src = x0[src];
     for (unsigned dst = src; dst < n; dst++) {
-      const double r = unif(prng);
+      const float r = unif(prng);
       const int x0ij = x0src * x0[dst];
-      if ((x0ij == +1 && r <= inp) || (x0ij == -1 && r <= outp)) {
+      if (unlikely((x0ij == +1 && r <= inp) || (x0ij == -1 && r <= outp))) {
         entries.emplace_back(src, dst, 1);
-        if (src != dst)
+        if (likely(src != dst))
           entries.emplace_back(dst, src, 1);
       }
     }
