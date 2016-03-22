2492214f337885237e1da997f672fb2e8e8c8377
Stephen Tu <stephent@berkeley.edu>
2016-03-22 12:06:14 -0700

    fix bug in driver

--- src/cpp/driver.py
+++ src/cpp/driver.py
@@ -9,6 +9,19 @@ import itertools as it
 import multiprocessing as mp
 import subprocess
 
+import errno
+import os
+
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
 
 def parameters(n, d, lam):
     """Given (n, d, lam), try to return an a, b consistent.
@@ -33,19 +46,20 @@ def parameters(n, d, lam):
 def goOnce(idx, n, d, lam, trial, a, b, seedSample, seedOpt):
     cmd = "./instance --n {} --r {} --a {} --b {} --seed-gen {} --seed-opt {}".format(n, 100, a, b, seedSample, seedOpt)
     print cmd
-    subprocess.call(cmd + " > {}.json".format(idx), shell=True)
+    subprocess.call(cmd + " > results/{}.json".format(idx), shell=True)
 
 
 def main():
     lambdas = [1.0, 1.005, 1.01, 1.015, 1.02, 1.025, 1.03]
     ds = [5.0]
     ns = [16000]
-    trials = 1
+
+    def ntrials(n): return int(np.ceil(6.4e8 / float(n)))
 
     seenSampleSeeds, seenOptSeeds = set(), set()
 
-    def grid():
-        for idx, (n, d, lam, trial) in enumerate(it.product(ns, ds, lambdas, range(trials))):
+    def grid(ns, ds, lambdas, trials):
+        for n, d, lam, trial in it.product(ns, ds, lambdas, range(trials)):
             a, b = parameters(n, d, lam)
 
             seedSample = np.random.randint(low=0, high=0xFFFFFFFF)
@@ -58,10 +72,17 @@ def main():
                 seedOpt = np.random.randint(low=0, high=0xFFFFFFFF)
             seenOptSeeds.add(seedOpt)
 
-            yield idx, n, d, lam, trial, a, b, seedSample, seedOpt
+            yield n, d, lam, trial, a, b, seedSample, seedOpt
 
-    Parallel(n_jobs=mp.cpu_count() / 2, backend='threading')(
-        delayed(goOnce)(*args) for args in grid())
+    fullGrid = it.chain(
+            grid([16000], [5.0], lambdas, ntrials(16000)),
+            grid([32000], [5.0], lambdas, ntrials(32000)))
+
+    jobs = [delayed(goOnce)(idx, *args) for idx, args in enumerate(fullGrid)]
+    print "njobs:", len(jobs)
+
+    mkdir_p("results")
+    Parallel(n_jobs=mp.cpu_count() / 2, backend='threading')(jobs)
 
 
 if __name__ == '__main__':
